20d727abd0e4ec570cf76602f80582f3ea43e5b6
hidekimaesaka <lucasmsk11@gmail.com>
2022-04-05 21:13:16 -0300

    Adiciona react-router-dom, Adiciona tela de superusuário

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import QuestionBlock from './components/QuestionBlock';
+import SuperUser from './components/SuperUser';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
+      <Router>
     <div className="App">
       <h1>Quest Game - FATEC SP</h1>
-      <QuestionBlock></QuestionBlock>
-    </div>
+      <Routes>
+          <Route path="/superuser" element={<SuperUser></SuperUser>}>
+          </Route>
+          <Route exact path="/" element={<QuestionBlock></QuestionBlock>}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
 export default App;
---
+++ src/components/SuperUser.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SuperUser(){
+    return(
+        <div className="SuperUser">
+            <h1>Eufdhguokdjflkghjdflgjkdflidgjkflsuário</h1>
+        </div>
+    )
+}
